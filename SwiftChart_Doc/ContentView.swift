fbd58307f572e42b87bf323138a51e9b5f85a156
muhmmad-zeeshan <rafaq657@gmail.com>
2024-11-30 18:26:00 +0500

    Initial Commit

---
+++ SwiftChart_Doc/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftChart_Doc
+//
+//  Created by Muhammad Zeeshan on 30/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftChart_Doc/SwiftChart_DocApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChart_DocApp.swift
+//  SwiftChart_Doc
+//
+//  Created by Muhammad Zeeshan on 30/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChart_DocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
